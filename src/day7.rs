7473e35e5c4228c50bfd0c0886117878e417b2e5
Leiv Erik Rosseland <leiv.rosseland@gmail.com>
2019-12-07 00:44:09 +0100

    getting ready for day 7

---
+++ src/day7.rs
@@ -0,0 +1,13 @@
+use crate::common::utils;
+
+pub fn part1() {
+    println!("Running day7 part1!");
+
+    println!("Completed day7 part1!\n");
+}
+
+pub fn part2() {
+    println!("Running day7 part2!");
+
+    println!("Completed day7 part2!\n");
+}
