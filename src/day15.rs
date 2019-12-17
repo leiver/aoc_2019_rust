6ff2efd49a28f3d30da07ec17293c60d9212093f
Leiv Erik Rosseland <leiv.rosseland@gmail.com>
2019-12-17 20:33:45 +0100

    completed day 14 part 1

--- src/day15.rs
+++ src/day15.rs
@@ -17,15 +17,62 @@ pub fn part1() {
         t_output_final.send(-1);
     });
 
-    let mut unexplored_tiles: HashMap<(i32,i32),(i32,(i32,i32,i32,i32))> = HashMap::new();
     let mut map: HashMap<(i32,i32),(i32,i32,i32,i32)> = HashMap::new();
-    unexplored_tiles.insert((0,0),(-1,(-1,-1,-1,-1)));
 
     let mut x = 0;
     let mut y = 0;
     let mut oxygen_system = (0,0);
+    let mut previous_direction = -1;
 
     while !unexplored_tiles.is_empty() {
+        let mut default_map_tile = (-1,-1,-1,-1);
+        if previous_direction != -1 {
+            default_map_tile[previous_direction] = 1;
+        } else {
+            previous_direction = 3;
+        }
+        let (north,east,south,west) = map.entry((x,y)).or_insert(default_map_tile);
+
+        for i in 1..5 {
+            let next_direction = (previous_direction + i) % 4;
+            if next_direction == 0 && *north == -1 {
+                t_input.send(1);
+                let output = r_output.recv().unwrap();
+                *north = output as i32;
+                if output != 0 {
+                    y -= 1;
+                    previous_direction = 2;
+                    break;
+                }
+            } else if next_direction == 1 && *east == -1 {
+                t_input.send(1);
+                let output = r_output.recv().unwrap();
+                *north = output as i32;
+                if output != 0 {
+                    y -= 1;
+                    previous_direction = 2;
+                    break;
+                }
+            } else if next_direction == 2 {
+                t_input.send(1);
+                let output = r_output.recv().unwrap();
+                *north = output as i32;
+                if output != 0 {
+                    y -= 1;
+                    previous_direction = 2;
+                    break;
+                }
+            } else if next_direction == 3 {
+                t_input.send(1);
+                let output = r_output.recv().unwrap();
+                *north = output as i32;
+                if output != 0 {
+                    y -= 1;
+                    previous_direction = 2;
+                    break;
+                }
+            }
+        }
 
     }
 
