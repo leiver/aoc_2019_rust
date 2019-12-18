e46f037ba12e4d5d928871f87c355d24b60e8569
Leiv Erik Rosseland <leiv.rosseland@gmail.com>
2019-12-18 07:48:22 +0100

    day 15 complete

--- src/day16.rs
+++ src/day16.rs
@@ -3,6 +3,15 @@ use crate::common::utils;
 pub fn part1() {
     println!("Running day16 part1!");
 
+    let initial_input = utils::read_whole_file("inputs/day16.txt");
+
+    let mut input: Vec<i32> = Vec::new();
+    for char in initial_input.chars() {
+        input.push(char.to_string().parse::<i32>().unwrap());
+    }
+
+
+
     println!("Completed day16 part1!\n");
 }
 
